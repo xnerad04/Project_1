4b7c15b7d6e15cde85567234eb60dbc1cfa7e3bf
xnerad04 <xnerad04@vutbr.cz>
2022-11-22 14:28:42 +0100

    Updated

--- project-01/src/main.c
+++ project-01/src/main.c
@@ -1,9 +1,62 @@
-#include <Arduino.h>
+/************************************************************************
+*
+* Test of the Pmod module
+*
+*************************************************************************
+* Description: Pmod_CLP
+* Message "Test module Pmod Digilent partner of Lextronic" will be show on LCD.
+*
+* Material
+* 1. Arduino Uno
+* 2. Pmod CLP
+*
+**************************************************/
+// Affectation of pins
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <lcd.h>            // Peter Fleury's LCD library
+#include <stdlib.h>         // C library. Needed for number conversions
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int main(void)
+{
+    // Initialize display
+    lcd_init(LCD_DISP_ON);
+    lcd_gotoxy(1, 0); lcd_puts("value:");
+    lcd_gotoxy(3, 1); lcd_puts("key:");
+    lcd_gotoxy(8, 0); lcd_puts("a");  // Put ADC value in decimal
+    lcd_gotoxy(13,0); lcd_puts("b");  // Put ADC value in hexadecimal
+    lcd_gotoxy(8, 1); lcd_puts("c");  // Put button name here
+
+    // Configure Analog-to-Digital Convertion unit
+    // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
+    ADMUX = ADMUX | (1<<REFS0);
+    // Select input channel ADC0 (voltage divider pin)
+    ADMUX = ADMUX & ~(1<<MUX3 | 1<<MUX2 | 1<<MUX1 | 1<<MUX0);
+    // Enable ADC module
+    ADCSRA = ADCSRA | (1<<ADEN);
+    // Enable conversion complete interrupt
+    ADCSRA = ADCSRA | (1<<ADIE);
+    // Set clock prescaler to 128
+    ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+
+    // Configure 16-bit Timer/Counter1 to start ADC conversion
+    // Set prescaler to 33 ms and enable overflow interrupt
+    TIM1_overflow_33ms();
+    TIM1_overflow_interrupt_enable();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines ISRs */
+    }
+
+    // Will never reach this
+    return 0;
 }
